5d34d03701f4b3cb2a53524ef6f965082e4c1bce
Chris Buonocore <chrisdistrict@gmail.com>
2022-09-23 21:00:58 -0400

    reorganize

--- setup.mac.sh
+++ setup.mac.sh
@@ -9,7 +9,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 echo "Starting setup"
 # Step 1: Update the OS
 
-echo "Installing homebrew, then various dependencies for Engineering."
+echo "Installing homebrew, then various dependencies for engineering."
 
 # Check for Homebrew,
 # Install if we don't have it
@@ -21,31 +21,35 @@ fi
 brew install coreutils
 sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
-# Install Python (for AWS tooling + ML)
-brew install python
-brew install python3
-
-brew install git
-brew install git-extras
-brew install tree
-
-brew install zsh
-
 # Install BE requirements
 brew install jq
 brew install docker
 brew install awscli
 
+# Python (for AWS tooling + ML)
+brew install python
+brew install pyenv
+
+# Front end
+brew install yarn
+
+
+# Git
+brew install git
+brew install git-extras
+brew install tree
+
+
+# Go
+brew install go
+
+# Misc
+brew install zsh
 brew install gcc
-brew install maven@3.5
-echo 'export PATH="/usr/local/opt/maven@3.5/bin:$PATH"' >> ~/.zshrc
-
 brew install tfenv
-tfenv install 0.11.7
-
 brew install httpie
 brew install fzf
-
 brew install vim
 
+# Cleanup
 brew cleanup
